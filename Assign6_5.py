459cbc336b2cd53c8ee9b68608fc695168bed21f
Priya24Rana <61672593+Priya24Rana@users.noreply.github.com>
2020-03-05 10:37:00 -0500

    Add files via upload

---
+++ Assign6_5.py
@@ -0,0 +1,20 @@
+import mydays
+
+num = int(input("enter the day you want : "))
+if num == 1 :
+    print ("What day it is? " ,"\nIt is " , mydays.days[1] )
+elif num == 2 :
+    print ("what day it is? " ,"\nIt is " ,  mydays.days[2])
+elif num == 3 :
+    print ("What day it is? " ,"\nIt is " ,  mydays.days[3])
+elif num == 4 :
+    print ("What day it is? " ,"\nIt is " ,  mydays.days[4])
+elif num == 5 :
+    print ("What day it is? " ,"\nIt is " ,  mydays.days[5])
+elif num == 6 :
+    print ("What day it is? " ,"\nIt is " , mydays.days[6])
+elif num == 7 :
+    print ("What day it is? " , "\nIt is " , mydays.days[7])
+else:
+    print ("Invalid Input")
+    
