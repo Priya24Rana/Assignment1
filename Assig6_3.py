95c649b9b330bcec9cbb9dfaf6ec9f2f56e653c7
Priya24Rana <61672593+Priya24Rana@users.noreply.github.com>
2020-03-05 10:33:19 -0500

    Add files via upload

---
+++ Assig6_3.py
@@ -0,0 +1,7 @@
+def function_convert():
+    Celsius = int(input("Enter a temperature in Celsius: "))
+    Fahrenheit =Celsius * 9/5 + 32
+    print ("Temperature:", Celsius, "Celsius = ", Fahrenheit, " F")
+
+function_convert()
+    
